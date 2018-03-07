e8ae60fc4670f13a60088560be8a26b361a8a4d5
Christophe Vauclair <cfvauclair@gmail.com>
2018-03-07 16:00:32 -0500

    Supplier info test

--- supplymap/app/controllers/supply_chain_controller.rb
+++ supplymap/app/controllers/supply_chain_controller.rb
@@ -1,4 +1,6 @@
 class SupplyChainController < ApplicationController
+    PER_PAGE = 10
+
     # This is the main api endpoint of our app for get requests...    
     def test 
         p "API request for supply chain!"
@@ -8,7 +10,15 @@ class SupplyChainController < ApplicationController
 
     def all_shops
         p "Requesting all shops"
-         shops = Shop.all
+        shops = Shop.all
         render json: {status: 'SUCCESS', message: 'Loaded all shops', data: shops}, status: :ok
-    end 
+    end
+
+    def suppliers
+        p "Requesting suppliers"
+        page_number = params[:page] ? params[:page].to_i : 1
+        supplier_offset = PER_PAGE * (page_number - 1)
+        @suppliers      = Supplier.limit(PER_PAGE).offset(supplier_offset)
+        @next_page  = page_number + 1 if @suppliers.count == PER_PAGE
+    end
 end
--- supplymap/app/models/supplier.rb
+++ supplymap/app/models/supplier.rb
@@ -1,2 +1,6 @@
-class Supplier < ApplicationRecord
+class Supplier < ActiveRecord::Base
+	validates :name, presence: true
+	validates :type, presence: true
+	validates :lattitude, presence: true
+	validates :longitude, presence: true
 end
