30c996dffd2184ef046d193f185988d8ce7d194e
Christophe Vauclair <cfvauclair@gmail.com>
2018-03-10 13:51:12 -0500

    Finished the supply chain admin API
    Added client side requests handling that return JSON formatted data
    from the database (see updated README.md sections admin API and client
    API for more details)

--- supplymap/config/routes.rb
+++ supplymap/config/routes.rb
@@ -2,10 +2,22 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   
-  #Example get route
-  get 'supply_chain/' => 'supply_chain#test'
-  get 'supply_chain/all_shops' => 'supply_chain#all_shops'
+  # home path
+  get 'home/' => 'home#index'
 
+  # JSON Data request paths (see data_controller.rb for implementation)
+  get 'all_products/' => 'data#all_products'
+  get 'all_shops/' => 'data#all_shops'
+  get 'all_suppliers/' => 'data#all_suppliers'
+  get 'all_supplychains/' => 'data#all_supplychains'
+
+  # Supply chains RESTful API (see supply_chains_controller.rb for controller and 
+  # views/supply_chains/ for html pages to show, modify and create new supply chains)
+  get 'supply_chains/' => 'supply_chains#index'
+  resources :supply_chains
+
+  # Suppliers RESTful API (see suppliers_controller.rb for controller and 
+  # views/suppliers/ for html pages to show, modify and create new suppliers)
   get 'suppliers/' => 'suppliers#index'
   resources :suppliers
 end
