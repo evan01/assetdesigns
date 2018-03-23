0d86580fc923fa6bf395def592b15b7f2e6ca84f
Jason Dias <dias.jason9@gmail.com>
2018-03-23 14:14:16 -0400

    updated supplier table

--- supplymap/app/models/supplier.rb
+++ supplymap/app/models/supplier.rb
@@ -2,4 +2,6 @@ class Supplier < ActiveRecord::Base
 	has_many(:supplier_connections, :foreign_key => :supplier_a_id, :dependent => :destroy)
 	has_many(:reverse_supplier_connections, :class_name => :SupplierConnection, :foreign_key => :supplier_b_id, :dependent => :destroy)
  	has_many :suppliers, :through => :supplier_connections, :source => :supplier_b
+	has_attached_file :image
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
