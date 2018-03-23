6d8ec9cb47add8083202cbba01711edd62279786
Jason Dias <dias.jason9@gmail.com>
2018-03-23 14:14:16 -0400

    updated supplier table

--- supplymap/app/models/supplier.rb
+++ supplymap/app/models/supplier.rb
@@ -5,4 +5,6 @@ class Supplier < ActiveRecord::Base
 	has_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
  	has_and_belongs_to_many :supply_chains
+	has_attached_file :image
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
