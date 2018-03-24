476e2869ce2118a5a444165d9898b56d5b060141
Jason Dias <dias.jason9@gmail.com>
2018-03-24 11:45:06 -0400

    updated schema + routes

--- supplymap/config/routes.rb
+++ supplymap/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :product_types
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
-  
+
   # home path
   get 'home/' => 'home#index'
 
@@ -14,15 +14,19 @@ Rails.application.routes.draw do
   get 'all_connections/' => 'data#all_connections'
   get 'all_data/' => 'data#all_data'
 
-  # Supply chains RESTful API (see supply_chains_controller.rb for controller and 
+  # Supply chains RESTful API (see supply_chains_controller.rb for controller and
   # views/supply_chains/ for html pages to show, modify and create new supply chains)
   get 'supply_chains/' => 'supply_chains#index'
   resources :supply_chains
 
-  # Suppliers RESTful API (see suppliers_controller.rb for controller and 
+  # Suppliers RESTful API (see suppliers_controller.rb for controller and
   # views/suppliers/ for html pages to show, modify and create new suppliers)
   get 'suppliers/' => 'suppliers#index'
-  resources :suppliers
+  resources :suppliers do
+    member do
+      delete 'remove_image'
+    end
+  end
 
   get 'supplier_connections/' => 'supplier_connections#index'
   resources :supplier_connections
